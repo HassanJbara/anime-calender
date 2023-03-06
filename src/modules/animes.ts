f1ee0e301b525b6e6ae66dba9c3a3903400c6e90
Hassan <hasan-account2012-3@live.com>
2023-03-06 18:02:30 +0300

    update Anime interface

--- src/modules/animes.ts
+++ src/modules/animes.ts
@@ -1,12 +1,22 @@
 export const seasons = ["WINTER", "SPRING", "SUMMER", "FALL"] as const;
+export const formats = ["TV", "OVA", "MOVIE", "SPECIAL"] as const;
+export const genres = ['COMEDY', 'ACTION', 'SHOUNEN', 'ISEKAI', 'SHOUJO', 'DRAMA', 'ROMANCE', 'SCHOOL', 'ADVENTURE', 
+                        'SLICE_OF_LIFE', 'MYSTERY', 'FANTASY', 'HAREM', 'MECHA', 'SCI-FI', 'SPORTS', 'ECCHI']
+
+
 export type season = (typeof seasons)[number];
+export type format = (typeof formats)[number];
+export type genre = (typeof genres)[number];
 
 export interface Anime {
     id: number;
     next_episode: string;
     year: number;
     name: string;
+    studio: string;
+    format: format;
     story: string;
+    genre: genre[];
     cover_image: string;
     season: season;
     start_date: string;
