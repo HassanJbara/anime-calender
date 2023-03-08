17de034099ac0d461434327bef10944408d3e216
Hassan <hasan-account2012-3@live.com>
2023-03-08 06:00:43 +0300

    fix eslint, update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "default-gray": "#adb5b9",
+      "summer-red": "#bf0f0f",
+      "fall-blue": "#173841",
+      "spring-wine": "#8D1F20",
+      "winter-torq": "#38A6A7",
+    },
   },
   plugins: [],
-}
+};
