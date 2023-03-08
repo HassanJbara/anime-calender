72eac5419d7f0d9776595bc1aab16c274b7b2a85
Hassan <hasan-account2012-3@live.com>
2023-03-08 16:55:20 +0300

    add necessary svgs, configure tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      "default-gray": "#adb5b9",
-      "summer-red": "#bf0f0f",
-      "fall-blue": "#173841",
-      "spring-wine": "#8D1F20",
-      "winter-torq": "#38A6A7",
+    extend: {
+      colors: {
+        "default-gray": "#adb5b9",
+        "summer-red": "#bf0f0f",
+        "fall-blue": "#173841",
+        "spring-wine": "#8D1F20",
+        "winter-torq": "#38A6A7",
+        "burger-text": "#4c5f66",
+      },
+      boxShadow: {
+        "default": "3px 3px 6px #00000029",
+        "menu": "0px 3px 10px #00000029",
+      },
     },
   },
   plugins: [],
