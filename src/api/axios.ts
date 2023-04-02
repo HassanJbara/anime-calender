523fd458aeeb88b34737790d08501dcc5cebf4e0
Hassan <hasan-account2012-3@live.com>
2023-04-02 20:20:16 +0200

    configure env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 export const axiosAPI = axios.create({
   withCredentials: false,
   timeout: 28000,
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
